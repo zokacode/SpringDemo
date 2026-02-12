7643fc677b08045eadfd61a0e512ff26391b0d17
k <kpro8899+git@protonmail.com>
2026-02-12 17:11:10 +0800

    feat: 新增 PersonRepository 介面

---
+++ src/main/java/com/example/repository/PersonRepository.java
@@ -0,0 +1,10 @@
+package com.example.repository;
+
+import com.example.model.PersonModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<PersonModel, Long> {
+
+}
